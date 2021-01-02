8eaa820ee23bf309b0f36807d0bccdfc8e11e005
ujin2021 <ujin2021@naver.com>
2021-01-02 20:07:42 +0900

    fix : Fix .gitignore

--- backend/config/database.js
+++
@@ -1,8 +0,0 @@
-const mysql = require('mysql2')
-
-module.exports = mysql.createConnection({
-    host: '13.124.208.47',
-    user: 'root',
-    password: 'svtcarat0526',
-    database: 'bucket_project'
-})
--- backend/config/jwt.js
+++
@@ -1,1 +0,0 @@
-exports.secretKey = '2dc7#vuk7m_!(7*5r0cfqrff91&@30gnul7ga%osq(!zu0j8yo'
