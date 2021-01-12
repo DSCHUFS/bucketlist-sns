5b46bd3e68bf69ba5edb144d2c2239377a2bb36c
bengHak <qkwl4678@naver.com>
2021-01-13 03:56:42 +0900

    feat : left sidebar ui

--- frontend/src/page/MainPage.js
+++ frontend/src/page/MainPage.js
@@ -1,10 +1,46 @@
 import React, { Component } from "react";
+import styled from "styled-components";
+import LeftSideBar from "../component/sidebar/LeftSideBar";
+
+const MainPageRoot = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  margin-top: 80px;
+  width: 100%;
+  height: calc(100vh - 80px);
+`;
+
+const TopNav = styled.div`
+  position: absolute;
+  top: 0;
+  width: 100%;
+  height: 80px;
+  background-color: grey;
+`;
+
+const RightSideBar = styled.div`
+  display: flex;
+  flex-direction: column;
+  right: 0;
+  width: 400px;
+  background-color: blue;
+`;
+
+const FeedDiv = styled.div`
+  width: 100%;
+`;
 
 class MainPage extends Component {
   render() {
     return (
       <>
-        <h1>헬로</h1>
+        <TopNav>상단바</TopNav>
+        <MainPageRoot>
+          <LeftSideBar>왼쪽</LeftSideBar>
+          <FeedDiv>메인 피드</FeedDiv>
+          <RightSideBar>오른쪽</RightSideBar>
+        </MainPageRoot>
       </>
     );
   }
