33072ab6c96451697d01cab0e9e6e47fd925edc2
ujin2021 <ujin2021@naver.com>
2021-01-12 16:06:04 +0900

    fix: Modify followingTags queries

--- backend/controller/tag.js
+++ backend/controller/tag.js
@@ -2,31 +2,28 @@ const express = require('express')
 const db = require('../config/database')
 const tagQuery = require('../queries/tag')
 
-const findTagId = async (tag_name) => {
-    let result = await db.promise().query(tagQuery.FIND_TAG_ID, [tag_name])
-    result = result[0][0]
-    return result
-}
-
+// 이미 있는 tag들 중 following/unfollowing
 exports.followingTagAPI = async(req, res) => {
     try {
         const user_id = res.user_id
         const { tag_name, following } = req.body
         const following_query = (following === 'unfollowing') ? tagQuery.UNFOLLOWING_TAG : tagQuery.FOLLOWING_TAG
 
-        let result = await findTagId(tag_name)
-        if(result) {
-            const tag_id = result.tag_id
-            console.log(`tag_id : ${tag_id}`)
-            result = await db.promise().query(following_query, [user_id, tag_id])
-            res.status(200).json({'msg' : `${following} ${tag_name}`})
-        } else {
-            throw 'invalid tag name'
-        }
+        await db.promise().query(following_query, [user_id, tag_name])
+        res.status(200).json({'msg' : `${following} ${tag_name}`})
     } catch(e) {
-        if(e === 'invalid tag name') {
-            res.status(400).json({'msg' : `invalid tag name`})
-        }
+        console.log(e)
+        res.status(400)
+    }
+}
+
+exports.userFollowingListAPI = async(req, res) => {
+    try{
+        const user_id = res.user_id
+        let result = await db.promise().query(tagQuery.FOLLOWING_LIST, [user_id])
+        const msg = (result[0].length === 0) ? `No following tags` : result[0].map(result => result.tag_name)
+        res.status(200).json({'msg':`${msg}`})
+    } catch(e) {
         console.log(e)
         res.status(400)
     }
--- backend/queries/tag.js
+++ backend/queries/tag.js
@@ -1,3 +1,3 @@
-exports.UNFOLLOWING_TAG = `DELETE FROM FollowingTags WHERE user_id = ? and tag_id = ?;`
-exports.FOLLOWING_TAG = `INSERT INTO FollowingTags(user_id, tag_id) VALUES(?, ?);`
-exports.FIND_TAG_ID = `SELECT tag_id FROM Tags WHERE tag_name = ?;`
+exports.UNFOLLOWING_TAG = `DELETE FROM FollowingTags WHERE user_id = ? and tag_name = ?;`
+exports.FOLLOWING_TAG = `INSERT INTO FollowingTags(user_id, tag_name) VALUES(?, ?);`
+exports.FOLLOWING_LIST = `SELECT tag_name FROM FollowingTags WHERE user_id = ?`
