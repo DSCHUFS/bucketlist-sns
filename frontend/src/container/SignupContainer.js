653e3bab3d9c9c6392a3cf2eaa9e65c8dfb2080c
ujin2021 <ujin2021@naver.com>
2021-01-12 22:27:21 +0900

    fix: Use redux

--- frontend/src/container/SignupContainer.js
+++ frontend/src/container/SignupContainer.js
@@ -1,44 +1,28 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import axios from 'axios'
 import SignupTemplate from '../component/signup/SignupTemplate'
 import SignupForm from '../component/signup/SignupForm'
+import { setInput } from '../action/signup'
 
 class SignupContainer extends Component {
-    constructor(props) {
-        super()
-        this.state = {
-        email: '',
-        pw: '',
-        pwCheck: '',
-        name: '',
-        birth: '',
-        death: '',
-        profile_image: '',
-        profile_text: '',
-        tags: ''
-        }
-    }
-
-    onChange = (e) => {
-        this.setState({[e.id]: e.value})
-    }
-
     onRegister = async () => {
         console.log('signup button click')
-        console.log(this.state)
+        console.log(this.props.userInfo)
         const postSignup = async() => {
-            const res = await axios.post('localhost:3000/signup', this.state)
-            console.log(`res : ${res}`)
+            const res = await axios.post('http://localhost:3001/signup', this.props.userInfo) // server랑 client port가 같아서 서버를 임의로 3001로 바꾸고 테스트
+            console.log(`res : ${JSON.stringify(res)}`)
         }
         postSignup()
     }
 
     render() {
+        const {onChange} = this.props
         return(
             <div style={{backgroundColor: '#e6e9f2'}}>
                 <SignupTemplate />
                 <SignupForm 
-                    onChange = { this.onChange }
+                    onChange = { onChange }
                     onRegister = { this.onRegister }
                 />
             </div>
@@ -46,4 +30,21 @@ class SignupContainer extends Component {
     }
 }
 
-export default SignupContainer
+const mapStateToProps = (state) => ({
+    userInfo: {
+        email: state.signup.email,
+        password: state.signup.pw,
+        name: state.signup.name,
+        birth: state.signup.birth,
+        death: state.signup.death,
+        profile_image: state.signup.profile_image,
+        profile_detail: state.signup.profile_text,
+        tag: state.signup.tags
+    }
+})
+
+const mapDispatchToProps = (dispatch) => ({
+    onChange: (e) => dispatch(setInput(e))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(SignupContainer)
