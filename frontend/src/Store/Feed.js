52554c944708014d9b2fe3256e5a5dc51ed47c8e
talksame <rnjsdndud4@gmail.com>
2021-01-13 18:50:04 +0900

    resource : Add FeedData

--- frontend/src/Store/Feed.js
+++ frontend/src/Store/Feed.js
@@ -1,4 +1,43 @@
 export const feeds = [
-
-  ];
-  
+  {
+    id: 0,
+    title : 'My Dream',
+    content: 'I want to work at Codddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddmpany',
+    like: 0,
+    dday: 0,
+    progress : false
+    
+  },
+  {
+    id: 1,
+    title : 'trip to Europ',
+    content: 'I want to trip to Swiss',
+    like: 0,
+    dday: 0,
+    progress : false
+  },
+  {
+    id: 2,
+    title : 'Buy My House ',
+    content: 'I want to Buy House with my effort',
+    like: 0,
+    dday: 0,
+    progress : false
+  },
+  {
+    id: 3,
+    title : '삼성알고리즘 대회 결승가기',
+    content: '삼성 알고리즘 대회 결승 가보기',
+    like: 0,
+    dday: 0,
+    progress : false
+  },
+  {
+    id: 4,
+    title : 'My Dream',
+    content: 'I want to work at Company',
+    like: 0,
+    dday: 0,
+    progress : false
+  },
+];
