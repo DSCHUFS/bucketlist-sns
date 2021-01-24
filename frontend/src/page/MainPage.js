9854c1a7823f145bcfd7e69b314ddad5f8fee74b
bengHak <qkwl4678@naver.com>
2021-01-24 14:14:53 +0900

    fix : resolve conflict

--- frontend/src/page/MainPage.js
+++ frontend/src/page/MainPage.js
@@ -1,14 +1,22 @@
 import React, { Component } from "react";
 import MainLayout from "../layout/MainLayout";
-import { withRouter } from 'react-router-dom';
+import { withRouter } from "react-router-dom";
+import FeedPage from "../component/page/FeedPage";
 
 class MainPage extends Component {
+  componentDidMount() {
+    // 확인
+  }
   render() {
-    const location = this.props.location
-    const token = (location.state) ? location.state.token : ''
+    const location = this.props.location;
+    const token = location.state ? location.state.token : "";
+    // key 검사 => 없으면 없는대로 처리
+    // 있으면 로그인
     return (
       <>
-        <MainLayout>hello</MainLayout>
+        <MainLayout>
+          <FeedPage />
+        </MainLayout>
       </>
     );
   }
