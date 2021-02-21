20bdbad6b32f19b2df23297051f2a3803817abb9
bengHak <qkwl4678@naver.com>
2021-02-21 16:54:39 +0900

    feat: update Profile

--- frontend/src/container/profile/ProfileContainer.js
+++ frontend/src/container/profile/ProfileContainer.js
@@ -4,38 +4,78 @@ import Profile from "../../component/profile/Profile";
 
 class ProfileContainer extends Component {
   state = {
-    user_info: null,
+    user_info: undefined,
+    following_tags: undefined,
   };
+
+  constructor(props) {
+    super(props);
+    this.getUserInfo = this.getUserInfo.bind(this);
+    this.getFollowingTags = this.getFollowingTags.bind(this);
+  }
+
   componentDidMount() {
+    this.getUserInfo();
+    this.getFollowingTags();
+  }
+
+  getUserInfo() {
     var config = {
       method: "get",
-      url: "http://localhost:3001/mypage",
+      url: `/user/${this.props.userId}`,
       headers: {
         Authorization: localStorage.getItem("token"),
       },
     };
 
-    let res = axios(config)
+    axios(config)
       .then((response) => {
-        return response.data.info;
+        this.setState({
+          ...this.state,
+          user_info: response.data.info,
+        });
       })
       .catch((error) => {
         console.log(error);
         return null;
       });
+  }
 
-    res.then((data) => {
-      this.setState({
-        ...this.state,
-        user_info: data,
+  getFollowingTags() {
+    var config = {
+      method: "get",
+      url: `/tag/following/list/${this.props.userId}`,
+      headers: {
+        Authorization: localStorage.getItem("token"),
+        "user-id": this.props.userId,
+      },
+    };
+
+    axios(config)
+      .then((response) => {
+        console.log(JSON.stringify(response.data.tags));
+        this.setState({
+          ...this.state,
+          following_tags: response.data.tags,
+        });
+      })
+      .catch(function (error) {
+        console.log(error);
       });
-    });
   }
 
   render() {
-    // console.log(this.state);
-    if (this.state.user_info !== null)
-      return <Profile userInfo={this.state.user_info} />;
+    console.log(this.state);
+    if (
+      this.state.user_info !== undefined &&
+      this.state.following_tags !== undefined
+    )
+      return (
+        <Profile
+          userInfo={this.state.user_info}
+          followingTags={this.state.following_tags}
+        />
+      );
     return null;
   }
 }
